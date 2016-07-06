ca2c8faa010e792522e7e731389b644dc9d609d1
Sakari Lehtonen <sakari@psitriangle.net>
2016-07-06 14:47:47 +0300

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,9 @@
 set -x EDITOR vim
-set -x PATH $PATH /usr/local/bin $HOME/bin /opt/local/bin /opt/local/sbin /usr/local/devkitpro/devkitARM/bin
+
+set -x PATH $PATH /usr/local/bin $HOME/bin /opt/local/bin /opt/local/sbin
 set -x NODE_PATH /opt/local/lib/node_modules
 set -x PYTHONPATH "$HOME/lib/python:$HOME/lib/python/PIL"
 set -x LC_CTYPE "fi_FI"
-set -x gk "/Users/sakari/dvl/geokone"
-set -x vr "/Users/sakari/dvl/Geometrify"
+
+set -x gk "$HOME/dvl/geokone"
+set -x gm "$HOME/dvl/geometrify"
