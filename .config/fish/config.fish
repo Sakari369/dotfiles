dde33240410285de5cedbfcf4687500d731ba9b7
Sakari Lehtonen <sakari@psitriangle.net>
2020-05-20 08:32:26 +0300

    Update fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,13 @@
 set -x EDITOR vim
 
-set -x PATH $PATH /usr/local/bin $HOME/bin /opt/local/bin /opt/local/sbin
+set -x PATH /opt/local/bin /usr/local/bin $HOME/dvl/ext/depot_tools/ $HOME/bin /opt/local/sbin /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin $HOME/.cargo/bin $PATH
 set -x NODE_PATH /opt/local/lib/node_modules
-set -x PYTHONPATH "$HOME/lib/python:$HOME/lib/python/PIL"
-set -x LC_CTYPE "fi_FI"
+set -x LC_CTYPE "fi_FI.UTF-8"
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home"
+set -x LIBRARY_PATH "/opt/local/lib"
 
-set -x gk "$HOME/dvl/geokone"
+set -x omg "$HOME/dvl/omg"
+set -x cfg "$HOME/Library/Application Support/omnigeometry"
 set -x gm "$HOME/dvl/geometrify"
+set -x psi "$HOME/dvl/PSIEngine"
+set -x nv "$HOME/dvl/omg/neurovizr"
